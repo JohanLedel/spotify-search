d5996721b3f98d7ea827eb2506c54a0370919de8
JohanLedel <83391297+JohanLedel@users.noreply.github.com>
2022-11-24 21:38:24 +0100

    eslint fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../App';
 
-export default function () {
+export default function Header() {
     const { user, setUser } = useContext(UserContext),
         signOut = () => {
             setUser(null);
         },
         redirect_uri =
             'https://spotify-search-teal.vercel.app/callback',
-        redirect_uri_dev = 'http://localhost:3000/callback',
+        //redirect_uri_dev = 'http://localhost:3000/callback',
         authorizeUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${redirect_uri}`;
 
     return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { search } from '../utils/utils';
 import SearchResults from '../components/SearchResults';
 import { UserContext } from '../App';
