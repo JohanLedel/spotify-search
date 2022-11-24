70c77f61d19ba0b0be0108a036a3def117c375e8
JohanLedel <83391297+JohanLedel@users.noreply.github.com>
2022-11-24 20:01:26 +0100

    Removed files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Header from './components/Header';
-import Search from './pages/Search';
 import Callback from './pages/Callback';
 
 export const UserContext = React.createContext(null);
@@ -18,7 +17,6 @@ function App() {
                 <Router>
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/search" element={<Search />} />
                         <Route exact path="/callback" element={<Callback />}>
                             <Route exact path=":code" element={<Callback />} />
                         </Route>
